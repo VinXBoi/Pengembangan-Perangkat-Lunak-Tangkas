7fc16be1e5d79f7253c446bca5b7b971717c5e08
Alvin Dinata <alvin.dinata@students.mikroskil.ac.id>
2025-03-25 08:50:47 +0700

    update uva

--- uva/uva.cpp
+++ uva/uva.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define INF 1000000000
 #define blank ' '
-#define len(x) x.size()
+#define len(x) (int) x.size()
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> ii;
@@ -20,23 +20,23 @@ string s;
 vector<char> player, dealer, temp;
 
 int checkCard(char a) {
-    if(a == 'A') return 1;
-    if(a == 'J') return 2;
-    if(a == 'Q') return 3;
-    if(a == 'K') return 4;
+    if(a == 'A') return 4;
+    if(a == 'J') return 1;
+    if(a == 'Q') return 2;
+    if(a == 'K') return 3;
     return 0;
 }
 
 void solve() {
     int turn = 0;
-    for(int i = 1; i < (int) len(s); i += 3) {
+    for(int i = 1; i < len(s); i += 3) {
         if(turn % 2 == 0) player.push_back(s[i]);
         else dealer.push_back(s[i]);
         turn++;
     }
     for(int i = 0; i < 3; i++) {
         getline(cin, s);
-        for(int j = 1; j < (int) len(s); j += 3) {
+        for(int j = 1; j < len(s); j += 3) {
             if(turn % 2 == 0) player.push_back(s[j]);
             else dealer.push_back(s[j]);
             turn++;
@@ -44,28 +44,36 @@ void solve() {
     }
 
     turn = 0;
-
-    while(len(player) > 0 && len(dealer) > 0) {
+    cout << "START\n";
+    while(true) {
+        if(turn % 2 == 0 && player.empty()) break;
+        if(turn & 1 == 0 && dealer.empty()) break;
         char a;
         bool found = false;
+        cout << "Turn : " << (turn % 2 == 0 ? "Player " : "Dealer ");
         if(turn % 2 == 0) a = *(player.end() - 1), player.pop_back();
         else a = *(dealer.end() - 1), dealer.pop_back();
-        
+        cout << a << endl;
         int take = checkCard(a);
+        temp.push_back(a);
+
         (take ? found = true : 0);
         turn++;
-        
+
         for(int i = 0; i < take; i++) {
+            if(player.empty() || dealer.empty()) break;
             if(turn % 2 == 0) a = *(player.end() - 1), player.pop_back();
             else a = *(dealer.end() - 1), dealer.pop_back();
-            if(checkCard(a)) i = 0, take = checkCard(a), turn++;
+            if(checkCard(a)) i = -1, take = checkCard(a), turn++;
+            temp.push_back(a);
         }
         if(found) {
+            turn++;
+            reverse(temp.begin(), temp.end());
             if(turn % 2 == 0) player.insert(player.begin(), temp.begin(), temp.end());
             else dealer.insert(dealer.begin(), temp.begin(), temp.end());
-            temp.clear()
+            temp.clear();
         }
-        temp.push_back(a);
     }
     if(len(player)) cout << 2 << ' ' << len(player) << endl;
     else cout << 1 << ' ' << len(dealer) << endl;
@@ -80,9 +88,16 @@ int main() {
 }
 
 /*
-HA H3 H4 CA SK S5 C5 S6 C4 D5 H7 HJ HQ
-D4 D7 SJ DT H6 S9 CT HK C8 C9 D6 CJ C6
-S8 D8 C2 S2 S3 C7 H5 DJ S4 DQ DK D9 D3
-H9 DA SA CK CQ C3 HT SQ H8 S7 ST H2 D2
-#
+H1 H1 
+H1 H1
+H1 H1
+H1 HJ
+
+p = 4; d = 4;
+p = 3; d = 3;
+p = 2; d = 6;
+d = 5; p = 1;
+d = 4; p = 0;
+d = 3; p = lose;
+
 */
